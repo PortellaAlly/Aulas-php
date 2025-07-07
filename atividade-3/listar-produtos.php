b42d3ccfc6fc5e800ea4e7a618d340570f9b2f00
Portella <Allysonpalmeirim12@gmail.com>
2025-07-07 17:39:48 -0300

    Update listar-produtos.php

--- atividade-3/listar-produtos.php
+++ atividade-3/listar-produtos.php
@@ -3,21 +3,20 @@
     $admin = "root";
     $senha = "";
     $nome_bd = "estoque";
-    $conexao = mysqli_connect($local, $admin, $senha, $nome_bd);
-    if (!$conexao) {
-        echo("Erro na conexão.");
-    }
 
-    $sql = "SELECT * FROM produto";
+    // conexão com o BD
+    $conexao = mysqli_connect($local, $admin, $senha, $nome_bd);
+
+    $sql = "SELECT * FROM produto"; // Operação sendo realizada
     $resultado = mysqli_query($conexao, $sql);
 
     if (mysqli_num_rows($resultado) > 0) {
         echo "<h2>Lista de Produtos</h2>";
-        echo "<table border='1' cellpadding='10'>";
-        echo "<tr><th>Nome</th><th>Valor</th><th>Quantidade</th></tr>";
+        echo "<table>";
+        echo "<tr>    <th>Nome</th>    <th>Valor</th>    <th>Quantidade</th>    </tr>";
         while ($linha = mysqli_fetch_assoc($resultado)) {
             echo "<tr>";
-            echo "<td>" . htmlspecialchars($linha["nome"]) . "</td>";
+            echo "<td>" . $linha["nome"] . "</td>";
             echo "<td>R$ " . number_format($linha["valor"], 2, ',', '.') . "</td>";
             echo "<td>" . $linha["qtd"] . "</td>";
             echo "</tr>";
